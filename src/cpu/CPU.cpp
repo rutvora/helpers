dcd5eff1b9d58c17988b78379bcd7e4d399ff643
Rut Vora <contactme@rutvora.com>
2023-12-12 20:25:27 -0800

    Redirect output from system call

--- src/cpu/CPU.cpp
+++ src/cpu/CPU.cpp
@@ -7,6 +7,8 @@
 #include "log/Logging.h"
 #include "helper.h"
 
+extern Logging *logging;  // Defined in main.cu
+
 namespace CPU {
 bool setCPUAffinity(const std::vector<uint8_t> &cpus) {
   if (cpus.empty()) return true;
@@ -27,7 +29,12 @@ bool setCPUAffinity(const std::vector<uint8_t> &cpus) {
 void moveToCSetShield() {
   // Move this process inside the shielded group of cores
   std::stringstream command;
-  command << "cset shield --shield --pid " << getpid();
-  system(command.str().c_str());
+  command << "cset shield --shield --pid " << getpid() << "> /dev/null 2>&1";
+  if (system(command.str().c_str()) != 0) {
+    if (logging != nullptr)
+      logging->log(Logging::ERROR, "CSet shield", "Could not move process to shielded group of cores");
+    else
+      std::cerr << "Could not move process to shielded group of cores" << std::endl;
+  }
 }
 }
